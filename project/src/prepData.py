9e219c8da1bf00ac1e2ea090fe0c5eb2cc950ed0
BINDAL Vivek <vivek.bindal@soprasteria.com>
2019-05-06 17:54:41 +0530

    90% acc; var/para/english/french

---
+++ project/src/prepData.py
@@ -0,0 +1,121 @@
+import numpy as np
+import random
+import json
+
+
+from keras.utils.np_utils import to_categorical
+
+PATH = "D:\\ML\\label descriminate\\data\\"
+JSON_PATH = PATH + "JSON\\"
+
+CATEGORY_DICT = {}
+CATEGORY_DICT["para"] = 0
+CATEGORY_DICT["var"] = 1
+CATEGORY_DICT["english"] = 2
+CATEGORY_DICT["french"] = 3
+
+
+CATEGORY_COUNT = len(CATEGORY_DICT.keys())
+MAX_INPUT_SIZE = 200000
+
+MAX_LENGTH = 32
+MAX_FEATURES = 80		#updated after fetching actual encoding
+
+
+
+def getData(inputFileName):
+	f = open(PATH+inputFileName+".txt")
+	l = [e.rstrip().lower() for e in f]
+	f.close()
+	
+	random.shuffle(l)
+	l = l[:MAX_INPUT_SIZE]
+	
+	tl = [(i,CATEGORY_DICT[inputFileName]) for i in l]
+	
+	return tl
+
+
+def getAllData():
+	data = []
+	for fileName in CATEGORY_DICT.keys():
+		data = data + getData(fileName)
+		
+	random.shuffle(data)
+
+	features = [i for i,j in data]
+	labels = [j for i,j in data]
+	
+	return features, labels
+
+
+def getEncoding(features):
+	distinctChar = []
+	for rec in features:
+		for chr in rec:
+			if chr not in distinctChar:
+				distinctChar.append(chr)
+				
+	distinctChar = sorted(distinctChar)
+	
+	charEncode = {ch: (index+1) for index,ch in enumerate(distinctChar)}
+	
+	MAX_FEATURES = len(charEncode)
+	return charEncode
+	
+	
+def saveDataEncoding():	
+	features,labels = getAllData()
+	encoding = getEncoding(features)
+	
+	
+	featureJson = json.dumps(features)
+	with open(JSON_PATH+"features.json", "w") as json_file:
+		json_file.write(featureJson)
+
+	labelJson = json.dumps(labels)
+	with open(JSON_PATH+"labels.json", "w") as json_file:
+		json_file.write(labelJson)
+		
+	encodeJson = json.dumps(encoding)
+	with open(JSON_PATH+"encoding.json", "w") as json_file:
+		json_file.write(encodeJson)
+		
+	
+	
+def vectorize(features,charEncode):
+	vectFeatures = np.zeros((len(features), MAX_LENGTH))		#initialize zero array
+	
+	for i,rec in enumerate(features):
+		for j,ch in enumerate(rec):
+			try:
+				vectFeatures[i][j] = charEncode[ch]
+			except KeyError:
+				print("Huh? " + ch)
+		
+	return vectFeatures
+	
+	
+def loadVectorizedData():
+	with open(JSON_PATH+"features.json", "r") as json_file:
+		features = json.load(json_file)
+		
+	with open(JSON_PATH+"labels.json", "r") as json_file:
+		labels = json.load(json_file)
+		
+	with open(JSON_PATH+"encoding.json", "r") as json_file:
+		encoding = json.load(json_file)
+	
+	
+	MAX_FEATURES = len(encoding)
+	
+	x = vectorize(features,encoding)
+	y = to_categorical(labels)
+		
+	return x,y
+	
+
+if __name__ == "__main__":
+	saveDataEncoding()
+	
+	
---
+++ project/src/train.py
@@ -0,0 +1,68 @@
+import numpy as np
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+import prepData as PD
+
+
+TRAIN_SPLIT = 10000
+
+PATH = PD.PATH
+MODEL_PATH = PATH + "models\\"
+
+
+def getData():
+	x,y = PD.loadVectorizedData()
+		
+	xTrain = x[:TRAIN_SPLIT]
+	xTest = x[TRAIN_SPLIT:]
+	yTrain = y[:TRAIN_SPLIT]
+	yTest = y[TRAIN_SPLIT:]
+	
+	return xTrain,yTrain,xTest,yTest
+
+
+def trainConvModel(xTrain,yTrain):	
+	model = Sequential()
+	model.add(layers.Embedding(PD.MAX_FEATURES, 64, input_length=PD.MAX_LENGTH))
+	model.add(layers.Conv1D(8, 7, activation='relu'))
+	model.add(layers.MaxPooling1D(5))
+	model.add(layers.Conv1D(8, 5, activation='relu'))
+	model.add(layers.GlobalMaxPooling1D())
+	model.add(layers.Dense(PD.CATEGORY_COUNT, activation='softmax'))
+	model.summary()
+	model.compile(	optimizer=RMSprop(lr=8e-4),
+					loss='categorical_crossentropy',
+					metrics=['acc'])
+	history = model.fit(xTrain, yTrain,
+					epochs=20,
+					batch_size=128,
+					validation_split=0.2)
+	
+	
+	saveModel(model, "label_model")
+	
+	return model
+	
+
+def saveModel(model,modelName):
+	# serialize model to JSON
+	model_json = model.to_json()
+	with open(MODEL_PATH + modelName + ".json", "w") as json_file:
+		json_file.write(model_json)
+	
+	# serialize weights to HDF5
+	model.save_weights(MODEL_PATH + modelName + ".h5")
+	print("Saved model to disk")
+	
+
+	
+if __name__ == "__main__":
+	xTrain,yTrain,xTest,yTest = getData()
+	model = trainConvModel(xTrain,yTrain)
+
+	#scores = model.evaluate(xTest, yTest, verbose=0)
+	#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
